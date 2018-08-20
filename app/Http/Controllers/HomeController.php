579027a8dd4bba370ae5d665886904f12c0d81ea
tuhindewan <tuhinsshadow@gmail.com>
2018-08-20 00:50:05 -0800

    product show on frontend

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Category;
 use App\Manufacture;
+use App\Product;
 
 class HomeController extends Controller
 {
     public function index(){
     	$categories = Category::getAllActiveCategories();
     	$manufactures = Manufacture::getAllActiveManufactures();
-    	return view('pages.main_content')->with(compact('categories','manufactures'));
+    	$products = Product::getAllActiveProducts();
+    	return view('pages.main_content')->with(compact('categories','manufactures','products'));
     }
 }
--- app/Product.php
+++ app/Product.php
@@ -27,5 +27,9 @@ class Product extends Model
             ->update(['publication_status' => 1]);
     }
 
+    public static function getAllActiveProducts(){
+        return DB::table('tbl_products')->join('tbl_category', 'tbl_products.category_id', '=', 'tbl_category.category_id')->join('tbl_manufacture', 'tbl_products.manufacture_id', '=', 'tbl_manufacture.manufacture_id')->select('tbl_products.*','tbl_category.category_name','tbl_manufacture.manufacture_name')->where('tbl_products.publication_status',1)->limit(9)->get();
+    }
+
 
 }
